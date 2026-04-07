5766d83885455ee717b193b16b1db40f16f1a177
DKR1688 <t.deepakrajput1688@gmail.com>
2026-04-07 15:22:40 +0530

    [Deepak] Add.Added UC15 to address book

---
+++ src/main/java/com/address_book_app/controller/HealthCheck.java
@@ -0,0 +1,13 @@
+package com.address_book_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+	@GetMapping("/health")
+	public String health() {
+		return "okk hai app";
+	}
+}
